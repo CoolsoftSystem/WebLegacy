f22062cfa05952b4613291011b173cd950a3218a
Gonzalo Fonzalida <fonzalidagonzalo@gmail.com>
2024-07-07 22:32:32 -0300

    fix primera subida

--- application/config/database.php
+++ application/config/database.php
@@ -74,6 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 //PRODUCCION
+/*
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
@@ -95,11 +96,11 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);
+);*/
 
 
 //TEST
-/*
+
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
@@ -121,4 +122,4 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);*/
+);
--- application/views/admin/cliente/vlist.php
+++ application/views/admin/cliente/vlist.php
@@ -1,7 +1,7 @@
 <div class="content-wrapper">
     <section class="content-header">
         <h1>
-            Clientes
+            Clienteeees
             <small>Listado de Clientes</small>
         </h1>
     </section>
