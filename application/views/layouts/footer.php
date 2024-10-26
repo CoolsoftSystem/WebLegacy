00bc1baac45cdac4c274ef9ab93ff1f9ec249261
Gabriel Garbi <gabrielgarbijob@gmail.com>
2024-10-26 13:12:08 -0300

    prueba de eliminar modal

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -288,41 +288,9 @@ $('.sidebar-menu').tree()
 $(".insertParte").on("click", function(e){
            e.preventDefault(); // cancela accion de href
            var ruta =$(this).attr("href");
-           //alert(ruta);
-           id=ruta.substring(ruta.lastIndexOf('/') + 1);
-         
-           swal({
-             title: "Esta por agregar Nueva Tarea",
-             //text: "Orden Nro° " +ruta.substring(ruta.lastIndexOf('/') + 1),
-             type: "warning",
-             showCancelButton: true,
-             confirmButtonColor: '#d9534f',
-             cancelButtonColor: '#d33',
-             confirmButtonText: "Confirmar",
-             showLoaderOnConfirm: true,
-             cancelButtonText: "Cancelar",
-             closeOnConfirm: false,
-             closeOnCancel: false
-              },
-              function(isConfirm){
-                if (isConfirm) {
-                  $.ajax({
-                    url: ruta,
-                    type: "POST",
-                    success:function(res){
                       res='mantenimiento/cparteorden/listar/';
                       window.location.href=base_url+res+id;
-                    }
-                  });
-                }else{
-                  swal({
-                      type: 'error',
-                      title: 'Cancelado',
-                      showConfirmButton: false,
-                      timer: 500
-                    });
-                }
-              });
+                    }               
          });
 //ACCIONES DE PARTE ***************************************************************************************
 
