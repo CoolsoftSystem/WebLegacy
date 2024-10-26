d31c6a7c2250a0f80507a0754d319a4ec6fd5bf3
Gabriel Garbi <gabrielgarbijob@gmail.com>
2024-10-26 12:11:57 -0300

    arreglo de tareas

--- application/views/admin/parteorden/vedit.php
+++ application/views/admin/parteorden/vedit.php
@@ -101,15 +101,16 @@
                                 <label for="txtgastos">Gastos</label>
                                 <input type="string" id="txtgastos" name="txtgastos" value="<?php echo !empty(form_error('txtgastos'))? set_value('txtgastos') : $Gastos ?>" class= "form-control"   disabled>
                             </div>
-                            <div class="col-md-1 form-group">
-                                <label>Completa</label><br>
-                                <input class="chk_input" type="checkbox" id="habilitado" name="habilitado" data-width="20" data-height="20" disabled <?=(!empty($parteordenedit->Completa)&&$parteordenedit->Completa=="1")?'checked':''?> <?=(!empty($consultar)) ? "disabled" : "";?> <?=(!isset($parteordenedit->Completa))?'checked':''?> />
-                                <span class="checkmark"></span>
-                            </div>                    
-                            <div class="col-sm-6 form-group">
+                                       
+                            <div class="col-sm-5 form-group">
                                 <label for="tarea">Tarea</label>
                                 <input type="text" id="txttarea" name="txttarea" maxlength="1000" value="<?php echo !empty(form_error('txtctarea'))? set_value('txttarea') : $parteordenedit->TareaDesarrollada ?>" class= "form-control" required >
                             </div>
+                            <div class="col-md-1 form-group">
+                                <label>Marcar como completa</label><br>
+                                <input class="chk_input" type="checkbox" id="habilitado" name="habilitado" data-width="20" data-height="20" disabled <?=(!empty($parteordenedit->Completa)&&$parteordenedit->Completa=="1")?'checked':''?> <?=(!empty($consultar)) ? "disabled" : "";?> <?=(!isset($parteordenedit->Completa))?'checked':''?> />
+                                <span class="checkmark"></span>
+                            </div>        
 
                     </div>
               </div>
